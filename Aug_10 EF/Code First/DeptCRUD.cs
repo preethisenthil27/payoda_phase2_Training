dfc73f62f0ab2e60c59d660be8ef6c1929cf824b
preethisenthil27 <141032679+preethisenthil27@users.noreply.github.com>
2023-08-12 22:41:12 +0530

    Add files via upload

---
+++ Aug_10 EF/Code First/DeptCRUD.cs
@@ -0,0 +1,44 @@
+﻿using EFCF.Models;
+using EFCF.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCF
+{
+    internal class DepartmentCRUD
+    {
+       
+        public void AddNewDept()
+        {
+            using (var context = new EmployeeMgmt())
+            {
+                var dept = new Dept()
+                {
+                    Deptno = 13,
+                    Deptname = "Hr"
+                };
+                context.Depts.Add(dept);
+                context.SaveChanges();
+            }
+        }
+
+        public void UpdateDept() 
+        { 
+            var context = new EmployeeMgmt();
+            var seldept = context.Depts.FirstOrDefault(d => d.Deptno == 10);
+            seldept.Deptname = "Dev";
+            context.SaveChanges();
+        }
+
+        public void DeleteDept() 
+        {
+            var context = new EmployeeMgmt();
+            var seldept = context.Depts.FirstOrDefault(d => d.Deptno == 10);
+            context.Depts.Remove(seldept);
+            context.SaveChanges();
+        }
+    }
+}
---
+++ Aug_10 EF/Code First/Program.cs
@@ -0,0 +1,13 @@
+﻿using EFCF;
+
+//Department CRUD Operations
+DepartmentCRUD Deptcrud = new DepartmentCRUD();
+Deptcrud.AddNewDept();
+Deptcrud.UpdateDept();
+Deptcrud.DeleteDept();
+
+//Employee CRUD Operations
+EmployeeCRUD employeeCRUD = new EmployeeCRUD();
+employeeCRUD.AddNewEmployee();
+employeeCRUD.UpdateEmployee();
+employeeCRUD.DeleteEmployee();
